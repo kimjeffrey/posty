b339800fb1bcd8704606d13f8f322665672c7c7a
Jeffrey Kim <jeffreyk93@gmail.com>
2021-01-26 16:47:48 -0500

    Initial commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\PostLiked;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+
+class DashboardController extends Controller
+{
+    // public function __construct() {
+        
+    // }
+
+    public function index() {
+        return view('dashboard');
+    }
+}
---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+    public function index() {
+        $posts = Post::orderBy('created_at', 'desc')->with('user', 'likes')->paginate(4);
+
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
+    }
+
+    public function show(Post $post) {
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
+
+    public function store(Request $request) {
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        $request->user()->posts()->create([
+            'body' => $request->body
+        ]);
+
+        return back();
+    }
+
+    public function destroy(Post $post) {
+        $this->authorize('delete', $post);
+        $post->delete();
+
+        return back();
+    }
+}
